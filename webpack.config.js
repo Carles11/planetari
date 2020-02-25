6a7ac00f408df8bb993a7ad120ac440656001422
Carles <carles@crix.design>
2020-02-25 15:09:39 +0100

    app up and running

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,56 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+// require('babel-register');
+
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|ico|gif|ttf)$/,
+        use: [{ loader: 'url-loader', options: { limit: 5000 } }],
+      },
+      {
+        test: /\.mp4$/,
+        use: 'file-loader',
+      },
+    ],
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'Planetari mòbil',
+      template: './public/index.html',
+      filename: 'index.html',
+      hash: true,
+      favicon: './src/assets/images/favicon.ico',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  watch: true,
+  devtool: 'source-map',
 };
